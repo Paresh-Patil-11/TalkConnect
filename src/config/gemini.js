7e89b112dbafd7a46ed39f1c297d23c7ea8ec76a
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-03 19:18:48 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const apiKey = import.meta.env.VITE_API_KEY;
+console.log("API Key:", apiKey);
+
 const genAI = new GoogleGenerativeAI(apiKey, { apiVersion: "v1" });
 const model = genAI.getGenerativeModel({
   model: "gemini-pro", 
